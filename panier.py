e1626a30a8514a708ae36ada4a532db0c360f9d8
lvdEphec <l.vandormael@ephec.be>
2025-11-24 10:17:38 +0100

    Ajoute la logique de réduction conditionnelle

--- panier.py
+++ panier.py
@@ -1,2 +1,7 @@
+nombre_reduction = 50
+valeur_reduction = 0.10
+
 def calculer_total(prix, quantite):
+    if quantite >= nombre_reduction:
+        return prix * quantite * (1 - valeur_reduction)
     return prix * quantite
